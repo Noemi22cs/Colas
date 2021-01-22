af38d936dfb416f2b367c993e7264cb18b36c7d0
Noemi22cs <77586866+Noemi22cs@users.noreply.github.com>
2021-01-22 04:07:51 -0600

    Add files via upload

---
+++ cola.py
@@ -0,0 +1,13 @@
+cola = ["Lola","Susana","Juana","Maura","Valentín","Andrés"]
+
+#El método append nos sirve para agregar nuevos elementos a la cola
+
+cola.append ("Eduardo")
+cola.append ("Judith")
+
+#El método pop nos sirve para visualizar al primer elemento de la cola
+
+primero = cola.pop(0)
+print(f"La primera en salir será {primero} y posteriormente serán : ")
+
+print (cola)
